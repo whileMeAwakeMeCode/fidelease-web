39dce8c9c9fad0c4c70e3fea7bbe21f419d28749
Pixsale <pixsale@protonmail.com>
2023-04-03 11:09:42 +0200

    mobile clientsView fully implem

--- src/ClientsView.js
+++ src/ClientsView.js
@@ -20,6 +20,8 @@ import { TiInfoLargeOutline } from "react-icons/ti";
 import { smoothScroll } from "./utils";
 import Layout from "./Layout";
 
+const { isSmallDevice } = Layout;
+
 
 export default class ClientsView extends Component {
     state = {
@@ -35,27 +37,42 @@ export default class ClientsView extends Component {
         const {loaded} = this.state;
         return <div className="flex">
             <div className="flex screensize" ref={section2}>
-                <div className='flex row allwidth height95 justifystart'>
+                {/* <div className='flex row allwidth height95 justifystart'> */}
+                <div className={`flex allwidth height95 ${isSmallDevice  ? '' : 'row justifystart'}`}>
                     <Fade left>  
                     
-                        <div className='flex flexdot4 justifystart' style={{ height: '70vh'}}>
-                            <div className='title flex text-shadow height90 justifystart'>
-                                    <img alt='' className="img-shadow" src={fLogo} style={{ width: '5vw', height: 'auto', objectFit: 'contain' }} /> 
-                                    {/* FIDELEASE */}
-                                    <img alt='' src={fText} style={{ width: '10vw', height: 'auto', objectFit: 'contain' }} />
-                                {/* <span className="primary">LICENCE CLIENT</span> */}
-                            </div>
-                            <div className="flex flexdot6">
-                                <div className="justifystart alignstart">
-                                <Fade right when={loaded}>
-                                    <img alt='' src={physicalCard} style={{ width: '20vw', height: 'auto', objectFit: 'contain', position: "absolute", right: '-5%', top: '25%' }} />
-                                </Fade>
-                                </div>
+                        {
+                            isSmallDevice
+                            // ? <div className='title flex text-shadow height90 justifystart'>
+                            //     <img alt='' className="img-shadow" src={fLogo} style={{ width: '15vw', height: 'auto', objectFit: 'contain' }} /> 
+                            //     <img alt='' src={fText} style={{ width: '25vw', height: 'auto', objectFit: 'contain' }} />
+                            // </div>
+                            ? <div>
                                 <Fade left>
-                                <img alt='' src={mobileApp} style={{ height: '50vh', width: 'auto', objectFit: 'contain', position: "absolute", left: '-15%' }} />
+                                    <img alt='' src={mobileApp} style={{ height: 'auto', width: '85vw', objectFit: 'contain', position: 'relative', left: '-7.5vw' }} />
                                 </Fade>
+                                {/* <Fade right when={loaded}>
+                                    <img alt='' src={physicalCard} style={{ width: '70vw', height: 'auto', objectFit: 'contain', position: 'relative', right: '-16vw' }} />
+                                </Fade> */}
                             </div>
-                        </div>
+                            
+                            : <div className='flex flexdot4 justifystart' style={{ height: '70vh'}}>
+                                <div className='title flex text-shadow height90 justifystart'>
+                                        <img alt='' className="img-shadow" src={fLogo} style={{ width: '5vw', height: 'auto', objectFit: 'contain' }} /> 
+                                        <img alt='' src={fText} style={{ width: '10vw', height: 'auto', objectFit: 'contain' }} />
+                                </div>
+                                <div className="flex flexdot6">
+                                    <div className="justifystart alignstart">
+                                        <Fade right when={loaded}>
+                                            <img alt='' src={physicalCard} style={{ width: '20vw', height: 'auto', objectFit: 'contain', position: "absolute", right: '-5%', top: '25%' }} />
+                                        </Fade>
+                                    </div>
+                                    <Fade left>
+                                        <img alt='' src={mobileApp} style={{ height: '50vh', width: 'auto', objectFit: 'contain', position: "absolute", left: '-15%' }} />
+                                    </Fade>
+                                </div>
+                            </div>
+                        }
                     </Fade>
                     <Fade right>  
                         <div className='flex flexdot5 allheight padded1'>
@@ -69,12 +86,12 @@ export default class ClientsView extends Component {
                             <div className='flex flexdot9 justifystart allheight width95'>
                                 {/* <ImMobile /> */}
                                 {/* TABLE */}
-                                <div className='flex allwidth height80'>
+                                <div className={`flex height80 ${Layout.isSmallDevice ? 'width90' : 'allwidth'}`}>
                                     <div className="bordered marged-t allwidth soft-bg box-shadow">
-                                        <div className="flex row allwidth" style={{borderBottom: '1px solid silver', paddingBottom: '1%'}}>
-                                            <div className="flex flexdot4 bold">Options</div>
-                                            <div className="flex flexdot3 bold" style={{borderRight: '1px solid silver', borderLeft: '1px solid silver'}}>Fidélisation classique</div>
-                                            <div className="flex flexdot3 bold">Programme Fidelease</div>
+                                        <div className="flex row allwidth" style={{borderBottom: '1px solid silver', paddingBottom: '1%', fontSize: Layout.isSmallDevice ? 'x-small' : ''}}>
+                                            <div className="flex flexdot6 bold">Options</div>
+                                            <div className="flex flexdot2 bold" style={{borderRight: '1px solid silver', borderLeft: '1px solid silver'}}>Fidélisation classique</div>
+                                            <div className="flex flexdot2 bold">Programme Fidelease</div>
                                         </div>
 
                                         {/* ItemRow */}
@@ -101,12 +118,21 @@ export default class ClientsView extends Component {
 
                             </div>
 
+                            {
+                                isSmallDevice
+                                && <div style={{marginTop: '5%'}}>
+                                    <Fade right when={loaded}>
+                                        <img alt='' src={physicalCard} style={{ width: '80vw', height: 'auto', objectFit: 'contain', position: 'relative', right: '-11vw' }} />
+                                    </Fade>
+                                </div>
+                            }
+
                             <div className="flex allwidth marged-t justifyend">
                                 {/* <button  onClick={() => smoothScroll(section3)}>En savoir plus</button> */}
                                 <Button
                                     title="En savoir plus"
                                     icon={<TiInfoLargeOutline />}
-                                    className="app-button marged-t xlarge"
+                                    className={`app-button marged-t ${Layout.isSmallDevice ? 'large marged-b' : 'xlarge'}`}
                                     onClick={() => smoothScroll(section3)}
                                 />
                             </div>
@@ -125,10 +151,10 @@ export default class ClientsView extends Component {
             <div className='flex screensize' ref={section3} style={{background: '#404040', color: '#fff', paddingTop: 20, paddingBottom: 40}}>
                 <div className="flex allspace justifystart width80">
                     <div className="flexy title xxlarge"> Avouez-le, la plupart de vos cartes de fidélité n'ont pas une grande valeur à vos yeux...</div>
-                    <div className="flex bold smaller">
+                    <div className={`flex bold ${isSmallDevice ? 'large marged-t' : 'smaller'}`}>
                         ÇA VA CHANGER AVEC 
-                        <div className=""><img className="inverted-img" src={fText} style={{width: Layout.isSmallDevice ? '30vw' : '15vw', height: 'auto', objectFit: 'contain'}}/></div>
-                        <img alt='' src={clientShopsList} style={{ width: Layout.isSmallDevice ? '50vw' : '15vw', height: 'auto', objectFit: 'contain' }} />
+                        <div className=""><img className="inverted-img" src={fText} style={{width: Layout.isSmallDevice ? '40vw' : '15vw', height: 'auto', objectFit: 'contain'}}/></div>
+                        <img alt='' src={clientShopsList} style={{ width: Layout.isSmallDevice ? '70vw' : '15vw', height: 'auto', objectFit: 'contain' }} />
                     </div>
                     <div className={`flex ${Layout.isSmallDevice ? 'width80' : 'width60'}`}>
                         {/* Terminé les portefeuilles gonflés de cartes de fidélité, on veut gonfler nos portefeuilles avec du pouvoir d'achat ! */}
